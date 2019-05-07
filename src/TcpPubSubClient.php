d7ed5f73f50ec25ee35b75259eba7da59debd6d5
Benjámin J. Ferge <benjamin.ferge17@gmail.com>
2019-05-07 05:39:20 +0200

    Fix TCP client freeze on publish

--- src/TcpPubSubClient.php
+++ src/TcpPubSubClient.php
@@ -2,33 +2,31 @@
 
 namespace PubSub;
 
-use React\Socket\ConnectionInterface;
-use React\EventLoop\Factory;
-use React\Socket\Connector;
-
 class TcpPubSubClient implements PubSubClient
 {
-    private $loop;
-    private $connector;
     private $addr;
     private $port;
 
     public function __construct(string $addr = "127.0.0.1", $port = 8080)
     {
-        $loop = Factory::create();
-        $this->loop = $loop;
-        $this->connector = new Connector($loop);
         $this->addr = $addr;
         $this->port = $port;
     }
 
     public function publish(string $topic, string $data)
     {
-        $host = $this->addr . ":" .  $this->port;
-        $this->connector->connect($host)->then(function (ConnectionInterface $conn) use ($topic, $data) {
-            $msg = new TcpPubSubMessage($topic, $data);
-            $conn->write((string)$msg);
-        });
-        $this->loop->run();
+        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        if ($socket === false) {
+            throw new \Exception("Socket creation failed: " . socket_strerror(socket_last_error()));
+        }
+
+        $result = socket_connect($socket, $this->addr, $this->port);
+        if ($result === false) {
+            throw new \Exception("Socket connection failed: ($result) " . socket_strerror(socket_last_error($socket)));
+        }
+
+        $msg = new TcpPubSubMessage($topic, $data);
+        socket_write($socket, $msg, strlen($msg));
+        socket_close($socket);
     }
 }
